b3af83aa14134a4710f64aab23b3c7421a91bcfe
PK <18bq1a12d2@vvit.net>
2024-04-29 12:45:27 +0530

    Exception file

--- src/exception.py
+++ src/exception.py
@@ -5,8 +5,8 @@ def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error messsage [{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error)
-    )
+        file_name,exc_tb.tb_lineno,str(error))
+    return error_message
 
 class CustomExcetion(Exception):
     def __init__(self,error_message,error_detail:sys):
